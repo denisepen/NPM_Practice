24fd6949abc845ba041fe49d51105770e2d2f305
Denise Pendleton-Lipinski <denisependletonlipinski@Denises-MacBook-Pro.local>
2018-10-15 11:51:20 -0400

    npm and faker knockknock catme packages

---
+++ app.js
@@ -0,0 +1,20 @@
+var cats = require("cat-me")
+var joke = require("knock-knock-jokes")
+var faker = require("faker");
+
+// console.log('from app.js');
+
+var randProduct = faker.commerce.productName()
+var price = faker.commerce.price()
+
+function productList(){
+  for (var i = 1; i <= 10; i++){
+    console.log(faker.fake("{{commerce.productName}} -  {{commerce.price}}"));
+  }
+}
+productList();
+
+
+// console.log(cats());
+//
+// console.log(joke());
